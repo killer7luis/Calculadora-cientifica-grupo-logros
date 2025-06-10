f23dc38c2beac4f07af3e59c7929d4ec1863afa7
killer7luis <luisgabrielnaranjofuenmayor@gmail.com>
2025-06-10 19:16:24 -0400

    yea

---
+++ Calculadora.cpp
@@ -0,0 +1,3 @@
+// Okay gente, tenemos que crear una calculadora cientifica.
+// Por favor con cada cambio coloquen un "//" y comenten que hace su codigo para que los otros
+// Sepan que fue lo que hicieron!
