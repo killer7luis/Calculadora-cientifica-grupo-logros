3d479e185572b8e5c1c07a032e2fd4ce1cf70e25
killer7luis <84632338+killer7luis@users.noreply.github.com>
2025-06-11 20:57:07 -0400

    Delete Calculadora.cpp

--- Calculadora.cpp
+++
@@ -1,3 +0,0 @@
-// Okay gente, tenemos que crear una calculadora cientifica.
-// Por favor con cada cambio coloquen un "//" y comenten que hace su codigo para que los otros
-// Sepan que fue lo que hicieron!
